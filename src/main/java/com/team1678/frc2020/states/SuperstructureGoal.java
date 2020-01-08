a18eb920a79eabbae29f67f4a02d5d7f2eb632e9
Mohamed E <mohamedapple@hotmail.com>
2020-01-08 09:28:17 -0800

    Add ss goal

---
+++ src/main/java/com/team1678/frc2020/states/SuperstructureGoal.java
@@ -0,0 +1,38 @@
+package com.team1678.frc2020.states;
+
+/**
+ * Represents a goal for the superstructure
+ */
+public class SuperstructureGoal {
+    public final SuperstructureState state;
+
+    public SuperstructureGoal(double turret, double shooter, double hood) {
+        this(new SuperstructureState(turret, shooter, hood);
+    }
+
+    public SuperstructureGoal(SuperstructureState state) {
+        this.state = new SuperstructureState(state);
+    }
+
+    public boolean equals(SuperstructureGoal other) {
+        return this.state.turret == other.state.turret &&
+                this.state.shooter == other.state.shooter &&
+                this.state.hood == other.state.hood;
+    }
+
+    public boolean isAtDesiredState(SuperstructureState currentState) {
+        double[] distances = {
+                currentState.turret - state.turret,
+                currentState.shooter - state.shooter,
+                currentState.hood - state.hood
+        };
+
+        for (int i = 0; i < distances.length; i++) {
+            if (Math.abs(distances[i]) > SuperstructureConstants.kPadding[i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
