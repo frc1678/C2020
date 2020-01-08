97862856eb14e00be4ee1cafb08f3aba6e53edf2
Mohamed E <mohamedapple@hotmail.com>
2020-01-08 09:32:35 -0800

    Add allowed error "padding" to ss constants

---
+++ src/main/java/com/team1678/frc2020/states/SuperstructureConstants.java
@@ -0,0 +1,11 @@
+package com.team1678.frc2020.states;
+
+public class SuperstructureConstants {
+    public static final double kTurretPaddingDegrees = 3;
+    public static final double kHoodPaddingDegrees = 2;
+    public static final double kShooterPaddingVelocity = 1;
+
+
+    public static final double[] kPadding = {
+            kTurretPaddingDegrees, kShooterPaddingVelocity, kHoodPaddingDegrees};
+}
