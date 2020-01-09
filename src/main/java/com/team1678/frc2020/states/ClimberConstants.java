a008599713382f32ca17ebec8063c7ac83e898be
Hanson <hzuoli@gmail.com>
2020-01-08 19:07:25 -0800

    Reformat constants

--- src/main/java/com/team1678/frc2020/states/ClimberConstants.java
+++ src/main/java/com/team1678/frc2020/states/ClimberConstants.java
@@ -1,13 +1,13 @@
 package com.team1678.frc2020.states;
 
 public class ClimberConstants { // TODO(Hanson) tune everything
-    public static double kElevatorMinHeight = 0.;
-    public static double kElevatorMaxHeight = 69.;
 
     // Godmode multiplier ratio
     public final static double kElevatorGodmodeThrottle = 60. / 50.;
 
     // elevator constants, in inches
+    public static double kElevatorMinHeight = 0.;
+    public static double kElevatorMaxHeight = 69.;
     public static double kGroundHeight = 0.;
     public static double kReachHeight = 63.;
     public static double kHookHeight = 60.;
