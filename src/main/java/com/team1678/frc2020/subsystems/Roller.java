6f02a66b34cfefab2379ccf203b8037dcb349dae
GeorgeBerdovskiy <gaberdovskiy@gmail.com>
2020-01-11 13:54:13 -0800

    Organize code, edit rotation/position control logic

--- src/main/java/com/team1678/frc2020/subsystems/Roller.java
+++ src/main/java/com/team1678/frc2020/subsystems/Roller.java
@@ -32,10 +32,10 @@ public class Roller extends Subsystem {
     // Color sensing
     private final ColorMatch mColorMatcher = new ColorMatch();
 
-    private final Color kBlueTarget = ColorMatch.makeColor(0.143, 0.427, 0.429);
-    private final Color kGreenTarget = ColorMatch.makeColor(0.197, 0.561, 0.240);
-    private final Color kRedTarget = ColorMatch.makeColor(0.561, 0.232, 0.114);
-    private final Color kYellowTarget = ColorMatch.makeColor(0.361, 0.524, 0.113);
+    private final Color kBlueTarget = ColorMatch.makeColor(0.250, 0.691, 0.633);
+    private final Color kGreenTarget = ColorMatch.makeColor(0.225, 0.604, 0.240);
+    private final Color kRedTarget = ColorMatch.makeColor(0.728, 0.410, 0.132);
+    private final Color kYellowTarget = ColorMatch.makeColor(0.871, 0.1194, 0.234);
     private Color mColorPositionTarget;
 
     // Detected when driving up - used to determine number of rotations
@@ -105,7 +105,7 @@ public class Roller extends Subsystem {
                     mColorPositionTarget = kYellowTarget;
                     break;
                 default :
-                    //This is corrupt data
+                    System.out.println("Invalid color from FMS!");
                     break;
             }
         } else {
@@ -131,54 +131,10 @@ public class Roller extends Subsystem {
             public void onLoop(double timestamp) {
                 synchronized (Roller.this) {
                     if (mRunningManual) {
+                        runStateMachine(false);
                         return;
                     } else {
-                        switch(mState) {
-                            case IDLE:
-                                mPeriodicIO.pop_out_solenoid = false;
-                                mPeriodicIO.roller_demand = 0.0;
-                                break;
-                            case ACHIEVING_ROTATION_CONTROL:
-                                mPeriodicIO.pop_out_solenoid = true;
-                                int i = 0;
-
-                                // Run roller until it has gone over the inital color four times
-                                while (i != 4) {
-                                    mPeriodicIO.roller_demand = kRotateVoltage;
-
-                                    if (colorHasChanged()) {
-                                        if (mPeriodicIO.detected_color == initialColor) {
-                                            i++;
-                                        }
-                                    }
-                                }
-
-                                // If it has run over the initial color four times, turn off roller
-                                if (i == 4) {
-                                    mPeriodicIO.roller_demand = 0;
-                                }
-
-                                break;
-                            case ACHIEVING_POSITION_CONTROL:
-                                if (mColorPositionTarget != null) {
-                                    mPeriodicIO.pop_out_solenoid = true;
-                                    ColorMatchResult match = mColorMatcher.matchClosestColor(mPeriodicIO.detected_color);
-
-                                    if (match.color == mColorPositionTarget) {
-                                        setState(WantedAction.NONE);
-                                    } else {
-                                        while (match.color != mColorPositionTarget) {
-                                            mPeriodicIO.roller_demand = kRotateVoltage;
-                                        }
-                                    }
-                                } else {
-                                    // There is no color goal, do nothing
-                                }
-
-                                break;
-                            default:
-                                break;
-                        }
+                        runStateMachine(true);
                     }
                 }
             }
@@ -190,6 +146,57 @@ public class Roller extends Subsystem {
             }
 
         });
+    }
+
+    // TODO: Only perform actions if 'modifyOutputs' is true
+    public void runStateMachine(boolean modifyOutputs) {
+        switch(mState) {
+            case IDLE:
+                mPeriodicIO.pop_out_solenoid = false;
+                mPeriodicIO.roller_demand = 0.0;
+                break;
+            case ACHIEVING_ROTATION_CONTROL:
+                mPeriodicIO.pop_out_solenoid = true;
+                int i = 0;
+
+                // Run roller until it has gone over the inital color four times
+                while (i < 4) {
+                    mPeriodicIO.roller_demand = kRotateVoltage;
+
+                    if (colorHasChanged()) {
+                        if (mPeriodicIO.detected_color == initialColor) {
+                            i++;
+                        }
+                    }
+                }
+
+                // If it has run over the initial color four times, turn off roller
+                if (i >= 4) {
+                    setState(WantedAction.NONE);
+                    i = 0;
+                }
+
+                break;
+            case ACHIEVING_POSITION_CONTROL:
+                if (mColorPositionTarget != null) {
+                    mPeriodicIO.pop_out_solenoid = true;
+                    ColorMatchResult match = mColorMatcher.matchClosestColor(mPeriodicIO.detected_color);
+
+                    if (match.color == mColorPositionTarget) {
+                        setState(WantedAction.NONE);
+                    } else {
+                        while (match.color != mColorPositionTarget) {
+                            mPeriodicIO.roller_demand = kRotateVoltage;
+                        }
+                    }
+                } else {
+                    // There is no color goal, do nothing
+                }
+
+                break;
+            default:
+                break;
+        }
     }
 
     public void zeroSensors() {}
