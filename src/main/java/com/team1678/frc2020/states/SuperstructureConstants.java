e8411f4ed28bd8d6339ca2cc3cb517e078bebf52
Mohamed E <mohamedapple@hotmail.com>
2020-01-12 14:25:01 -0800

    Add superstructure constants

--- src/main/java/com/team1678/frc2020/states/SuperstructureConstants.java
+++ src/main/java/com/team1678/frc2020/states/SuperstructureConstants.java
@@ -1,11 +1,63 @@
 package com.team1678.frc2020.states;
 
+import com.team254.lib.util.PolynomialRegression;
+import com.team254.lib.util.InterpolatingTreeMap;
+import com.team254.lib.util.InterpolatingDouble;
+
+
+
 public class SuperstructureConstants {
     public static final double kTurretPaddingDegrees = 3;
     public static final double kHoodPaddingDegrees = 2;
-    public static final double kShooterPaddingVelocity = 1;
+    public static final double kShooterPaddingVelocity = 100;
 
 
     public static final double[] kPadding = {
             kTurretPaddingDegrees, kShooterPaddingVelocity, kHoodPaddingDegrees};
+
+    //turret
+    public static final double kTurretDOF = 360;
+
+
+    //hood
+    public static double kDefaultHoodAngle = Math.toRadians(0);
+    public static boolean kUseHoodAutoAimPolynomial = true;
+
+    public static InterpolatingTreeMap<InterpolatingDouble, InterpolatingDouble> kHoodAutoAimMap = new InterpolatingTreeMap<>();
+    public static PolynomialRegression kHoodAutoAimPolynomial;
+
+    public static double[][] kHoodManualAngle = {
+        { 90.0, 60.0 }, // TODO: Fill in with values
+        { 140.0, 55.0},
+        
+    };
+
+    static {
+        //iterate through the array and place each point into the interpolating tree
+        for (double[] pair : kHoodManualAngle) {
+            kHoodAutoAimMap.put(new InterpolatingDouble(pair[0]), new InterpolatingDouble(pair[1]));
+        }
+        
+        kHoodAutoAimPolynomial = new PolynomialRegression(kHoodManualAngle, 1);
+    }
+    
+    //shooter
+    public static double kDefaultShootingRPM = 2950.0;
+    public static boolean kUseFlywheelAutoAimPolynomial = true;
+
+    public static InterpolatingTreeMap<InterpolatingDouble, InterpolatingDouble> kFlywheelAutoAimMap = new InterpolatingTreeMap<>();
+    public static PolynomialRegression kFlywheelAutoAimPolynomial;
+
+    public static double[][] kFlywheelManualRPM = {
+        { 90.0, 2890.0 }, // TODO: Fill in with values
+    };
+
+    static {
+        for (double[] pair : kFlywheelManualRPM) {
+            kFlywheelAutoAimMap.put(new InterpolatingDouble(pair[0]), new InterpolatingDouble(pair[1]));
+        }
+
+        kFlywheelAutoAimPolynomial = new PolynomialRegression(kFlywheelManualRPM, 2);
+    }
+
 }
