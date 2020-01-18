62c22f28b587546b0b5d702726ed43e51060807d
Mohamed E <mohamedapple@hotmail.com>
2020-01-18 15:51:48 -0800

    Update Controls
    *get rid of controlboard interfaces

--- src/main/java/com/team1678/frc2020/controlboard/IControlBoard.java
+++
@@ -1,7 +0,0 @@
-package com.team1678.frc2020.controlboard;
-
-public interface IControlBoard extends IDriveControlBoard, IButtonControlBoard {
-    boolean getRunIntake();
-
-	boolean getRunOuttake();
-}
