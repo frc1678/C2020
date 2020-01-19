e20a65dfdb838255fc11312ae2a8207c6d82b570
EithneA-V <eithne.arsuaga.vazquez@gmail.com>
2020-01-19 11:33:19 -0800

    Add comments to reflective log storage class

--- src/main/java/com/team1678/frc2020/logger/ReflectingLogStorage.java
+++ src/main/java/com/team1678/frc2020/logger/ReflectingLogStorage.java
@@ -6,19 +6,25 @@ public class ReflectingLogStorage<T> extends LogStorage {
     private Field[] mVars;
 
     public ReflectingLogStorage(Class<T> typeClass) {
-        mVars = typeClass.getFields(); //  getting member variables
+        //  Getting memeber variables
+        mVars = typeClass.getFields();
     }
     public ArrayList<String> getItemNames() {
+        //  create array list to the names of the member variables collected
     ArrayList<String> getItemNames = new ArrayList<String>();
+        //  For the member variables, get the variable  and add it to the array list
         for (Field variableName : mVars) {
             getItemNames.add(variableName.getName());
         }
+        //  Log item names from array list
         return getItemNames;
     }
+    //  Collect data from the variables and store in a new array list
     public void Add(T classData) {
         ArrayList<Double> newRow = new ArrayList<Double>();
         for (Field variableName : mVars) {
             try {
+                //  add data from the variables and make sure they're doubles to add them to the array list 
                 newRow.add(((Number)variableName.get(classData)).doubleValue());
             } catch (Exception e) {
                 System.err.print("Unable to add variable name data to new row as a double value");
