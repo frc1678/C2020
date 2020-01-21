a58b7240c69405d20d0f3747b82cc3bf36b60ee4
Mohamed E <mohamedapple@hotmail.com>
2020-01-20 21:51:00 -0800

    Clean up intake

--- src/main/java/com/team1678/frc2020/subsystems/Intake.java
+++ src/main/java/com/team1678/frc2020/subsystems/Intake.java
@@ -11,7 +11,6 @@ import com.team254.lib.util.ReflectingCSVWriter;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-
 public class Intake extends Subsystem {
     public static double kIntakingVoltage = 12.0;
     public static double kOuttakingVoltage = -12.0;
@@ -22,25 +21,25 @@ public class Intake extends Subsystem {
     public enum WantedAction {
         NONE, INTAKE, OUTTAKE,
     }
+
     public enum State {
         IDLE, INTAKING, OUTTAKING,
     }
+
     private State mState = State.IDLE;
 
-    private boolean mRunningManual = false;
-
     private static PeriodicIO mPeriodicIO = new PeriodicIO();
-    
+
     private final LazySparkMax mMaster;
 
     private ReflectingCSVWriter<PeriodicIO> mCSVWriter = null;
 
     public static class PeriodicIO {
-        //INPUTS
+        // INPUTS
         public double timestamp;
         public double current;
-        
-        //OUTPUTS
+
+        // OUTPUTS
         public double demand;
     }
 
@@ -79,25 +78,19 @@ public class Intake extends Subsystem {
             @Override
             public void onStart(double timestamp) {
                 // startLogging();
-                mRunningManual = false;
                 mState = State.IDLE;
             }
 
             @Override
             public void onLoop(double timestamp) {
                 synchronized (Intake.this) {
-                    if (mRunningManual) {
-                        runStateMachine(false);
-                        return;
-                    } else {
-                        runStateMachine(true);
-                    }
+                    runStateMachine();
+
                 }
             }
 
             @Override
             public void onStop(double timestamp) {
-                mRunningManual = false;
                 mState = State.IDLE;
                 stop();
                 stopLogging();
@@ -109,27 +102,20 @@ public class Intake extends Subsystem {
         return mState;
     }
 
-    public void runStateMachine(boolean modifyingOutputs) {
+    public void runStateMachine() {
         switch (mState) {
         case INTAKING:
-            if (modifyingOutputs) {
                 mPeriodicIO.demand = kIntakingVoltage;
-            }
             break;
         case OUTTAKING:
-            if (modifyingOutputs) {
                 mPeriodicIO.demand = kOuttakingVoltage;
-            }
             break;
         case IDLE:
-            if (modifyingOutputs) {
                 mPeriodicIO.demand = kIdleVoltage;
-            }
         }
     }
 
     public synchronized void setOpenLoop(double percentage) {
-        mRunningManual = true;
         mPeriodicIO.demand = percentage;
     }
 
@@ -138,7 +124,6 @@ public class Intake extends Subsystem {
     }
 
     public void setState(WantedAction wanted_state) {
-        mRunningManual = false;
         switch (wanted_state) {
         case NONE:
             mState = State.IDLE;
@@ -164,19 +149,22 @@ public class Intake extends Subsystem {
     public void writePeriodicOutputs() {
         mMaster.set(mPeriodicIO.demand / 12.0);
     }
+
     @Override
     public boolean checkSystem() {
         return true;
     }
+
     public synchronized void startLogging() {
         if (mCSVWriter == null) {
-            mCSVWriter = new ReflectingCSVWriter<>("/home/lvuser/INTAKE-LOGS.csv", PeriodicIO.class); 
+            mCSVWriter = new ReflectingCSVWriter<>("/home/lvuser/INTAKE-LOGS.csv", PeriodicIO.class);
         }
     }
+
     public synchronized void stopLogging() {
         if (mCSVWriter != null) {
             mCSVWriter.flush();
             mCSVWriter = null;
         }
     }
- }
+}
