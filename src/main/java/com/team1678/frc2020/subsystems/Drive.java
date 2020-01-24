b981d99a90864abfa24d9b333dde23f7fe4a836a
EithneA-V <eithne.arsuaga.vazquez@gmail.com>
2020-01-23 20:05:01 -0800

    fix imports

--- src/main/java/com/team1678/frc2020/subsystems/Drive.java
+++ src/main/java/com/team1678/frc2020/subsystems/Drive.java
@@ -9,7 +9,7 @@ import com.ctre.phoenix.sensors.PigeonIMU_StatusFrame;
 import com.team1678.frc2020.Constants;
 import com.team1678.frc2020.Kinematics;
 import com.team1678.frc2020.RobotState;
-import com.team1678.frc2020.logger.*;
+import com.team1678.frc2020.logger.LoggingSystem;
 import com.team1678.frc2020.logger.LogStorage;
 import com.team1678.frc2020.loops.ILooper;
 import com.team1678.frc2020.loops.Loop;
