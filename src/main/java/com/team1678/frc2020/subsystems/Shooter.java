d0a37c3e0813caba6527d7ee40173b2aea6d39ea
EithneA-V <eithne.arsuaga.vazquez@gmail.com>
2020-01-25 14:20:44 -0800

    Remove unused import

--- src/main/java/com/team1678/frc2020/subsystems/Shooter.java
+++ src/main/java/com/team1678/frc2020/subsystems/Shooter.java
@@ -7,7 +7,6 @@ import com.team1678.frc2020.logger.LogStorage;
 import com.team1678.frc2020.logger.LoggingSystem;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
 import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.TalonFXControlMode;
 
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
