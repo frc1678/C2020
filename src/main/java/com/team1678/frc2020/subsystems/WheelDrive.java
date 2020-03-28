c380f284b24f621b67dc0d4ab914d6d2c451bc98
Tom <treez.zhang@gmail.com>
2020-03-28 11:39:45 -0700

    frogot to save

--- src/main/java/com/team1678/frc2020/subsystems/WheelDrive.java
+++ src/main/java/com/team1678/frc2020/subsystems/WheelDrive.java
@@ -1,5 +1,6 @@
 package com.team1678.frc2020.subsystems;
 
+//import edu.wpi.first.wpilibj.PIDController;
 import com.team1678.lib.control.PIDController;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
@@ -8,25 +9,32 @@ public class WheelDrive extends Subsystem{
     private TalonFX motorSpeed;
     private PIDController pidController;
 
-    private final double maxV = 4.95; // MAX voltage
+    private final double kMaxV = 4.95; // MAX voltage
+
+    public WheelDrive(int motorAngle, int motorSpeed, int encoder) {
+        new TalonFX(motorAngle);
+        this.motorSpeed = new TalonFX(motorSpeed);
+
+        pidController = new PIDController(1, 0, 0);
+
+        //this version of PIDController was removed
+        //pidController.setOutputRange (-1, 1);
+        //pidController.setContinuous ();
+        //pidController.enable ();
+    }
 
     public void drive(double speed, double angle) {
 
         motorSpeed.set(ControlMode.PercentOutput, speed); // motorSpeed == speedMotor
 
-        double setpoint = angle * (maxV * 0.5) + (maxV * 0.5); // Calculate offset
+        double setpoint = angle * (kMaxV * 0.5) + (kMaxV * 0.5); // Calculate offset
         if (setpoint < 0) {
-            setpoint = maxV + setpoint;
+            setpoint = kMaxV + setpoint;
         }
-        if (setpoint > maxV) {
-            setpoint = setpoint - maxV;
+        if (setpoint > kMaxV) {
+            setpoint = setpoint - kMaxV;
         }
         pidController.setGoal(setpoint); // setGoal == setPoint
-    }
-
-    public WheelDrive(int motorAngle, int motorSpeed, int encoder) {
-        new TalonFX(motorAngle);
-        this.motorSpeed = new TalonFX(motorSpeed);
     }
 
     @Override
